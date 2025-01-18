f596166c2c322ad4c30767d187a08d5578756c5c
ayiakbarmaulana <opinigpt@gmail.com>
2025-01-18 19:52:35 +0700

    add set-x to kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,10 +4,13 @@ APP_NAME="my-app-1.0-SNAPSHOT.jar"
 
 PID=$(ps aux | grep "java -jar target/${APP_NAME}" | grep -v grep | awk '{print $2}')
 
+set -x
+
 if [ -z "$PID" ]; then
     echo "Aplikasi ${APP_NAME} tidak ditemukan."
 else
     echo "Menghentikan aplikasi ${APP_NAME} dengan PID: ${PID}..."
     kill -9 ${PID}
     echo "Aplikasi ${APP_NAME} dihentikan."
+    set +x
 fi
