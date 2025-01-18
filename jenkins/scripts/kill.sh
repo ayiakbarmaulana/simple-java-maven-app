cb736af7c0baf74c5ae94e85b5b947b5e158f7a3
ayiakbarmaulana <opinigpt@gmail.com>
2025-01-18 20:27:04 +0700

    update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
+
+echo 'The following command terminates the "my-app-1.0-SNAPSHOT.jar" process using its PID'
 APP_NAME="my-app-1.0-SNAPSHOT.jar"
 
 PID=$(ps aux | grep "java -jar target/${APP_NAME}" | grep -v grep | awk '{print $2}')
 
 set -x
 
-if [ -z "$PID" ]; then
-    echo "Aplikasi ${APP_NAME} tidak ditemukan."
-else
-    echo "Menghentikan aplikasi ${APP_NAME} dengan PID: ${PID}..."
-    kill -9 ${PID}
-    echo "Aplikasi ${APP_NAME} dihentikan."
-fi
+# if [ -z "$PID" ]; then
+#     echo "Aplikasi ${APP_NAME} tidak ditemukan."
+# else
+echo "Menghentikan aplikasi ${APP_NAME} dengan PID: ${PID}..."
+kill -9 ${PID}
+echo "Aplikasi ${APP_NAME} dihentikan."
+# fi
 
 set +x
